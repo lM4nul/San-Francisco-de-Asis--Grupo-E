c2c11bab899b3e9592d2d9fbb74690053a7ae6a7
AngelLagos <angelagosauceda@gmail.com>
2026-07-26 20:36:24 -0600

    feat: Implementacion de la pagina de Inicio y componente de tarjetas reutilizables

---
+++ src/components/cards/index.jsx
@@ -0,0 +1,4 @@
+export { default as Card } from './Card';
+export { default as CardHeaderImg } from './CardHeaderImg';
+export { default as CardBody } from './CardBody';
+export { default as CardFooter } from './CardFooter';
